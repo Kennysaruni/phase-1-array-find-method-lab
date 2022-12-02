bd8fbe50fedc7b05391fe04d3af41d87abf3b48f
Kennysaruni <kenny.saruni@student.moringaschool.com>
2022-12-02 20:55:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(DenverBroncos) {
+    for (let record of DenverBroncos) {
+      if (record.result === "W") {
+        return record.year;
+      }
+    }
+  }
+
+record.find(superbowlWin)
